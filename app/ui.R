abdd19419d2ebc3fb0784187859d183a0f97a01d
WenhuiF <98134604+WenhuiF@users.noreply.github.com>
2022-02-16 21:52:29 -0500

    Add files via upload

--- app/ui.R
+++ app/ui.R
@@ -1,16 +1,18 @@
-if (!require("shiny")) install.packages("shiny")
+#if (!require("shiny")) install.packages("shiny")
 library(shiny)
-if (!require("shinydashboard")) install.packages("shinydashboard")
+#if (!require("shinydashboard")) install.packages("shinydashboard")
 library(shinydashboard)
-if (!require("plotly")) { install.packages("plotly")}
+#if (!require("plotly")) { install.packages("plotly")}
 library(plotly)
-if(!require(fontawesome)) devtools::install_github("rstudio/fontawesome")
-if(!require(highcharter)) devtools::install_github("jbkunst/highcharter")
-if(!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-project.org")
-# if(!require(alberusa)) remotes::install_git("https://git.sr.ht/~hrbrmstr/albersusa")
-if (!require(tidyverse)) install.packages('tidyverse')
+#if(!require(fontawesome)) devtools::install_github("rstudio/fontawesome")
+#if(!require(highcharter)) devtools::install_github("jbkunst/highcharter")
+#if(!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-project.org")
+# if (!require(tidyverse)) install.packages('tidyverse')
 library(tidyverse)
 library(albersusa)
+library(leaflet)
+library(lubridate)
+library(googleVis)
 
 
 
@@ -21,38 +23,32 @@ shinyUI(dashboardPage(
   dashboardHeader(title ="NYC EXPRESS",titleWidth = 350),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Home",tabName = "Home", icon = icon("home")),
+      menuItem("Dashboard",tabName = "dashboard", icon = icon("dashboard")),
       
       menuItem("Map", tabName = "Map", icon = icon("chart-bar"),
                startExpanded = TRUE,
                menuSubItem("Covid Cases",tabName = "a"),
                menuSubItem("Dig by yourself",tabName = "b")),
-      menuItem("Analysis", tabName = "Analysis", icon = icon("tasks")),
       menuItem("Analysis", tabName = "Analysis", icon = icon("chart-bar"),
                startExpanded = TRUE,
                menuSubItem("Covid New Case", tabName = "New"),
                menuSubItem("NYC transportation",tabName = "both"),
                menuSubItem("Only Subways",tabName = "subway"),
                menuSubItem("Only Buses", tabName = "bus"))
-  )),
+          )
+  ),
   
     dashboardBody(
     tabItems(
-        tabItem(tabName = "Home",
+        tabItem(tabName = "dashboard",
                 h2("To be continued")
         ),
         
-        tabItem(tabName = "a",
-                box(width=12,
-                    h2('Map - Covid Cases per US State'),
-                    h4("The map shows the cumulative and death cases per state in US."),
-                    h4("Please click on the select box to explore by yourself."),
-                    br(),
-                    
+        tabItem(tabName = "a", 
               fluidPage(
                 
                 # Application title
-                # titlePanel("Map - Covid Cases per US State"),
+                titlePanel("Map - Covid Cases per US State"),
                 
                 
                 # Sidebar with a slider input for date
@@ -82,15 +78,15 @@ shinyUI(dashboardPage(
                               "2019_Census_US_Population_Data_By_State_Lat_Long"))
                   )
                 )
-              ))),
+              )),
               
               
       tabItem(tabName = "b", 
         fluidPage(
-          actionButton("free_meals","Free Meals Locations",icon=icon("utensils",  lib = "font-awesome"),style="color: #fff; background-color: #94a4db; border-color: #94a4db"),
-          actionButton("health","Mental Health Service",icon=icon("hospital",  lib = "font-awesome"),style="color: #fff; background-color: #94a4db; border-color: #94a4db"),
-          actionButton("textile","Textile Drop-Off Locations",icon=icon("tshirt",  lib = "font-awesome"),style="color: #fff; background-color: #94a4db; border-color: #94a4db"),
-          actionButton("job","Directory Of Job Centers",icon=icon("briefcase",  lib = "font-awesome"),style="color: #fff; background-color: #94a4db; border-color: #94a4db"),
+          actionButton("free_meals","Free Meals Locations",icon=icon("utensils",  lib = "font-awesome")),
+          actionButton("health","Mental Health Service",icon=icon("utensils",  lib = "font-awesome")),
+          actionButton("textile","Textile Drop-Off Locations",icon=icon("utensils",  lib = "font-awesome")),
+          actionButton("job","Directory Of Job Centers",icon=icon("utensils",  lib = "font-awesome")),
           br(),
           p("The plot won't update until the button is clicked.",
             " Without the use of ", code("isolate()"),
@@ -98,9 +94,48 @@ shinyUI(dashboardPage(
             " changes."),
           leafletOutput("mymap", width="100%", height=800))
       ),
-      tabItem(tabName = "Analysis",
-              h2("To be continued")
+      
+      
+      tabItem(tabName = "Interac_plot",
+              box(width=6,
+                  h4("The bar chart below shows that Brooklyn and Queens have the most positive antibody cases."),
+                  h4("Please click on the select box to see more specific distribution by borough."),
+                  br(),
+                  
+                  
+                  fluidPage(
+                    sidebarLayout(
+                      sidebarPanel(
+                        
+                        selectInput("count", 
+                                    label = "Antibody by Borough in Bar Plot",
+                                    choices = c("Number of Positive People" = 1,
+                                                "Number of People Tested" = 2,  
+                                                "Percentage of Positive Cases" = 3,
+                                                "Test Rate" = 4), 
+                                    selected = "Number of Positive People")),
+                      mainPanel(
+                        plotlyOutput("bar_plt")
+                      ))
+                  )
+                  
+              ),
+              
+              box(
+                width=6,
+                h4("Please click on the select box to see more specific distribution of positive antibody tests number & rate."),
+                br(),
+                
+                mainPanel(
+                  fluidRow(
+                    tabsetPanel(id = "tpanel",
+                                type = "tabs",
+                                tabPanel("number of test positive", plotlyOutput("plot1")),
+                                tabPanel("percent of test positive", plotlyOutput("plot2")))
+                  )
+                ))
       ),
+      
       tabItem(tabName = "subway", 
               fluidRow(htmlOutput("ggv_timeline_subway"), width=50, height=700)),
       
@@ -111,7 +146,6 @@ shinyUI(dashboardPage(
             fluidRow(htmlOutput("ggv_timeline"), width=50, height=700)),
     tabItem(tabName = "New",
             fluidRow(htmlOutput("ggv_timeline_new"), width=50, height=700))
-    )
-  )
+))
 ))
 
